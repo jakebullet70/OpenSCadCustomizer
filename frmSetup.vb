e97d0d23080428ded90e8ea36abe9863fb8a8edb
Steven De George SR <ehomecreations@gmail.com>
2024-02-28 09:00:54 +0200

    Added debug flag to view BATCH file

--- frmSetup.vb
+++ frmSetup.vb
@@ -2,14 +2,17 @@
 Public Class frmSetup
 
     Private m_SaveFolder, m_CadFolder, m_SlicerEXE As String
+    Private m_debug As Boolean
 
     Private Sub frmSetup_Load(sender As Object, e As EventArgs) Handles Me.Load
         txtSaveFolder.Text = My.Settings.savefolder
         txtCadFolder.Text = My.Settings.scadfolder
         txtSlicerEXE.Text = My.Settings.slicerEXE
+        chkDebug.Checked = My.Settings.debug
         m_CadFolder = txtCadFolder.Text
         m_SaveFolder = txtSaveFolder.Text
         m_SlicerEXE = txtSlicerEXE.Text
+        m_debug = chkDebug.Checked
     End Sub
 
     Private Sub btnPath2_Click(sender As Object, e As EventArgs) Handles btnPath2.Click
@@ -61,6 +64,7 @@ Public Class frmSetup
         My.Settings.savefolder = m_SaveFolder
         My.Settings.slicerEXE = m_SlicerEXE
         My.Settings.scadfolder = m_CadFolder
+        My.Settings.debug = m_debug
         My.Settings.Save()
         Me.Close()
     End Sub
@@ -71,6 +75,10 @@ Public Class frmSetup
 
     Private Sub txtCadFolder_Leave(sender As Object, e As EventArgs) Handles txtCadFolder.Leave
         m_CadFolder = txtCadFolder.Text
+    End Sub
+
+    Private Sub chkDebug_CheckedChanged(sender As Object, e As EventArgs) Handles chkDebug.CheckedChanged
+        m_debug = chkDebug.Checked
     End Sub
 
     Private Sub txtSaveFolder_Leave(sender As Object, e As EventArgs) Handles txtSaveFolder.Leave
